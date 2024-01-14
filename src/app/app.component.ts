3e58d83f41c618deb482a9614bcd10e99cb127b6
Sudaisneelambra <sudaisanuneelambra21@gmail.com>
2024-01-14 16:12:51 +0530

    hooks complited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hooks';
+  // title = 'hooks';
 
-  inputval:string=""
-  
-  constructor(){
+  // // inputval:string=""
+  // inputval:string[] = ["hello","hey dev"]
+
+  constructor() {
     console.log('app constructor triggered');
-    
   }
 
-  btnClick(input:HTMLInputElement){
+  // btnClick(input:HTMLInputElement){
 
-    this.inputval=input.value
+  //   this.inputval.push(input.value)
 
+  // }
+
+  // ------------------------------------------------------ngDoCheck------------------------------------------------------------------
+
+  // // inputval:string=""
+  // inputval:string=''
+
+  // btnClick(input:HTMLInputElement){
+
+  //   this.inputval=input.value
+
+  // }
+
+  // ------------------------------------------------------ngDoCheck------------------------------------------------------------------
+
+  // ------------------------------------------------------ngAfterContentCheck----------------------------------------------------------
+
+  // inputval:string=""
+  inputval: string = '';
+
+  btnClick(input: HTMLInputElement) {
+    this.inputval = input.value;
   }
+
+  // ------------------------------------------------------ngAfterContentCheck----------------------------------------------------------
+
+  // ------------------------------------------------------ngAfterViewInit-------------------------------------------------------
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit triggered in app component called');
+  }
+  // ------------------------------------------------------ngAfterViewInit-------------------------------------------------------
+
+  // -------------------------------------------------------ngAfterViewChecked-------------------------------------------------
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked triggered in app component called');
+  }
+  // -------------------------------------------------------ngAfterViewChecked-------------------------------------------------
 }
--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,29 +1,110 @@
-import { Component, Input, OnChanges, SimpleChange, SimpleChanges, } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  AfterContentChecked,
+  AfterContentInit,
+  Input,
+  OnChanges,
+  OnInit,
+  DoCheck,
+  SimpleChanges,
+  ViewChild,
+  ContentChild,
+  AfterViewInit,
+  AfterViewChecked,
+} from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
-  styleUrls: ['./demo.component.css']
+  styleUrls: ['./demo.component.css'],
 })
-export class DemoComponent implements OnChanges {
+export class DemoComponent {
+  // title:string="demo component"
+  // @Input()message!:string[]
 
+  // @ViewChild('temp') tempara:ElementRef;
 
-  title:string="demo component"
-  @Input()message!:string
-
-  constructor(){
+  constructor() {
     console.log('demo constructor triggered');
-    console.log(this.title);
-    console.log(this.message);
+    // console.log(this.title);
+    // console.log(this.message);
   }
 
-    ngOnChanges(changes: SimpleChanges): void {
-      console.log('ng on changes hook triggered');
-      console.log(this.message); 
-      console.log(changes);
-      
-    }
+  ngOnChanges(changes: SimpleChanges): void {
+    // console.log('ng on changes hook triggered');
+    // console.log(this.message);
+    // console.log(changes);
+  }
 
-  
+  ngOnInit() {
+    // in the ng on init time the view is not created yet
+    // console.log('ng OnInit hook triggered');
+    // console.log(this.tempara.nativeElement.innerHtml);
+  }
 
+  // ------------------------------------------------------------------------ngDoCheck-------------------------------------------------------------
+
+  // @Input()message!:string
+
+  ngDoCheck() {
+    // console.log("ngDocheck is triggered");
+    // console.log("inDoCheck",this.paracontent);
+  }
+
+  // ------------------------------------------------------------------------ngDoCheck-------------------------------------------------------------
+
+  // ---------------------------------------------------------------------ngAfterContentInit-------------------------------------------------------
+
+  // @Input()message!:string
+
+  // @ContentChild('temp')paracontent:ElementRef
+
+  ngAfterContentInit() {
+    // console.log("ngAfterContentInit triggered");
+    // console.log("inAfterContentInit",this.paracontent.nativeElement);
+  }
+
+  // ---------------------------------------------------------------------ngAfterContentInit-------------------------------------------------------
+
+  // -------------------------------------------------------------------ngAfterContentCheck-----------------------------------------
+
+  // @Input()message!:string
+
+  // @ContentChild('temp')paracontent:ElementRef
+
+  ngAfterContentChecked() {
+    //   console.log("ngAfterContentChecked triggered");
+    //   console.log("inAfterContentInit",this.paracontent.nativeElement);
+    // console.log("This from AfterContetChecked",this.tempara);
+  }
+
+  // -------------------------------------------------------------------ngAfterContentCheck-----------------------------------------
+
+  // ---------------------------------------------------------------------ngAfterViewInit----------------------------------------------
+
+  // @Input()message!:string
+
+  // @ViewChild('temp') tempara:ElementRef;
+
+  // @ContentChild('temp')paracontent:ElementRef
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit triggered');
+    // console.log("This from AfterVie Init",this.tempara);
+  }
+
+  // ---------------------------------------------------------------------ngAfterViewInit----------------------------------------------
+
+  // --------------------------------------------------------------ngAfterViewChecked-------------------------------------
+
+  @Input() message!: string;
+
+  @ViewChild('temp') tempara: ElementRef;
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked triggered');
+    console.log(this.tempara.nativeElement.textContent);
+  }
+
+  // --------------------------------------------------------------ngAfterViewChecked-------------------------------------
 }
